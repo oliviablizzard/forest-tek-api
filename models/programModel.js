19e8b0368c1e20383cc16c043db0b090ce971c48
Olivia Blizzard <olivialblizzard@gmail.com>
2024-10-31 11:20:58 -0300

    prepare for map

--- models/programModel.js
+++ models/programModel.js
@@ -3,14 +3,14 @@ import db from '../db/db.js';
 // fetch default list of programs 
 const getDefaultPrograms = (limit = 10) => {
     return db('programs')
-        .select('id', 'url', 'program_name', 'institution_name', 'province', 'image')
+        .select('id', 'url', 'program_name', 'institution_name', 'province', 'image', 'logo')
         .limit(limit);
 };
 
 // fetch programs filtered by province
 const getProgramsByProvince = (province) => {
     return db('programs')
-        .select('id', 'url', 'program_name', 'institution_name', 'province', 'image')
+        .select('id', 'url', 'program_name', 'institution_name', 'province', 'image', 'logo')
         .where('province', province);
 };
 
