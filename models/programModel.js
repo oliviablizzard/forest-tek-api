fa160442eb0ea0cb4f0847e1501dee1e7ac47f8d
Olivia Blizzard <olivialblizzard@gmail.com>
2024-10-30 21:42:26 -0300

    work on mapping

--- models/programModel.js
+++ models/programModel.js
@@ -3,14 +3,14 @@ import db from '../db/db.js';
 // fetch default list of programs 
 const getDefaultPrograms = (limit = 10) => {
     return db('programs')
-        .select('id', 'url', 'program_name', 'institution_name', 'province')
+        .select('id', 'url', 'program_name', 'institution_name', 'province', 'image')
         .limit(limit);
 };
 
 // fetch programs filtered by province
 const getProgramsByProvince = (province) => {
     return db('programs')
-        .select('id', 'url', 'program_name', 'institution_name', 'province')
+        .select('id', 'url', 'program_name', 'institution_name', 'province', 'image')
         .where('province', province);
 };
 
